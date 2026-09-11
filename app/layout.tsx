73920eb1be7275a817ad89d54d0c0bbd61accb6e
Ahmed Bdr <ahmed.bdr.ad.deen@gmail.com>
2026-09-11 19:07:08 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const rubik = localFont({
+  src: [
+    {
+      path: "../public/fonts/Rubik.ttf",
+    },
+  ],
+  variable: "--font-rubik",
 });
 
 export const metadata: Metadata = {
@@ -19,11 +18,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${rubik.variable} antialiased`}>
+      <body className="h-dvh w-dvw font-rubik">
+        <div className="">{children}</div>
+      </body>
     </html>
   );
 }
